9960af1960c1b8c4f4baacfbe47f2c189d987547
ShopiQR <101570866+ShopiQR@users.noreply.github.com>
2022-04-25 16:16:51 -0500

    nuevo diseño

--- assets/js/bs-init.js
+++ assets/js/bs-init.js
@@ -8,6 +8,7 @@ if (window.innerWidth < 768) {
 }
 
 document.addEventListener('DOMContentLoaded', function() {
+	AOS.init();
 
 	var products = document.querySelectorAll('[data-bss-dynamic-product]');
 
